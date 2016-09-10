081f8f9eafa76763c46526dfb49adaafac8633c4
Adarsh Kulkarni <akulkarni@Adarshs-MacBook-Pro-2.local>
2016-09-10 03:50:58 -0400

    MEAN App transition

---
+++ pennapps/app.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// New Code
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var monk = require('monk');
+var db = monk('localhost:27017/pennapps');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var Schema = mongoose.Schema;
+
+var symbolSchema = new Schema({
+    ticker: String,
+    name: String
+});
+
+var Symbol = mongoose.model('Symbol', symbolSchema)
+
+var portfolioSchema = new Schema({
+    companies = [String]
+});
+
+var Portfolio = mongoose.model('Portfolio', portfolioSchema)
+
+var companySchema = new Schema({
+    name: String,
+    ticker: String,
+    price: Number,
+    articles: [String],
+//    Add more aladdin properties.
+
+});
+
+var Company = mongoose.model('Company', companySchema)
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
